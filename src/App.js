c7c259f9823abd73750827b85e9c907f4f30fc35
Harshit10a <harshit10a@gmail.com>
2025-02-24 21:07:34 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup></LoginSignup>
+    </div>
+  );
+}
+
+export default App;
